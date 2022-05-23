d241148f4ca005b4d47e6ea0fe2c10f82e3b0c5f
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2022-05-23 14:50:40 -0600

    Implementando funcionalidad

--- client/src/Calculadora.jsx
+++ client/src/Calculadora.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 import './calculadora.css'
 
@@ -8,8 +8,48 @@ import { RiDivideLine } from 'react-icons/ri'
 
 const Calculadora = () => {
 
-	const prueba = '12,936'
-	const operacion = '308 x 42'
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const ops = ['/', '*', '+', '-', '.']
+
+	const updateCalc = (valor) => {
+		if (
+			calc.length > 8
+		) {
+			return
+		}
+		if (
+			ops.includes(valor) && calc === '' ||
+			ops.includes(valor) && ops.includes(calc.slice(-1))
+		) {
+			return
+		}
+		setCalc(calc + valor)
+
+		if (!ops.includes(valor)) {
+			setResult(Math.round(eval(calc + valor)).toString())
+		}
+	}
+
+	const calcular = () => {
+		const calculo = Math.round(eval(calc)).toString()
+		if (calculo < 0) {
+			setCalc("Error")
+			return
+		}
+		setCalc(calculo)
+	}
+
+	const borrarUltimo = () => {
+		if (calc == '') {
+			return
+		}
+		else {
+			const valor = calc.slice(0, -1)
+			setCalc(valor)
+		}
+	}
 
 	return (
 		<div className="calculadora_container">
@@ -17,13 +57,16 @@ const Calculadora = () => {
 			<div className="area_resultados">
 
 				<div className="barra_info">
-					{/* <div className="info_nombre"><p className='texto_nombre'> Andres Hernandez</p></div> */}
 					<div className="info_carnet"><p className='texto_carnet'>Andres Hernandez 20053</p></div>
 				</div>
 
 				<div className="barra_resultados">
-					<div className="texto_operacion">{operacion}</div>
-					<div className="texto_resultado">{prueba}</div>
+					{result
+						?
+						<div className="texto_operacion">({result})</div>
+						:
+						""}
+					<div className="texto_resultado">{calc || "0"}</div>
 				</div>
 
 
@@ -36,45 +79,53 @@ const Calculadora = () => {
 				<div className="fila">
 
 					<div className="boton parte_verde"><AiFillGithub /></div>
-					<div className="boton parte_verde">AC</div>
+					<div onClick={() => {
+						setCalc(prev => "")
+						setResult("")
+					}} className="boton parte_verde">AC</div>
 					<div className="boton parte_verde">%</div>
-					<div className="boton parte_roja"><RiDivideLine /></div>
+					<div onClick={() => { updateCalc('/') }} className="boton parte_roja"><RiDivideLine /></div>
 
 				</div>
 
 				<div className="fila">
 
-					<div className="boton">7</div>
-					<div className="boton">8</div>
-					<div className="boton">9</div>
-					<div className="boton parte_roja">X</div>
+					<div onClick={() => { updateCalc('7') }} className="boton">7</div>
+					<div onClick={() => { updateCalc('8') }} className="boton">8</div>
+					<div onClick={() => { updateCalc('9') }} className="boton">9</div>
+					<div onClick={() => { updateCalc('*') }} className="boton parte_roja">*</div>
 
 				</div>
 
 				<div className="fila">
 
-					<div className="boton">4</div>
-					<div className="boton">5</div>
-					<div className="boton">6</div>
-					<div className="boton parte_roja">-</div>
+					<div onClick={() => { updateCalc('4') }} className="boton">4</div>
+					<div onClick={() => { updateCalc('5') }} className="boton">5</div>
+					<div onClick={() => { updateCalc('6') }} className="boton">6</div>
+					<div onClick={() => { updateCalc('-') }} className="boton parte_roja">-</div>
 
 				</div>
 
 				<div className="fila">
 
-					<div className="boton">1</div>
-					<div className="boton">2</div>
-					<div className="boton">3</div>
-					<div className="boton parte_roja">+</div>
+					<div onClick={() => { updateCalc('1') }} className="boton">1</div>
+					<div onClick={() => { updateCalc('2') }} className="boton">2</div>
+					<div onClick={() => { updateCalc('3') }} className="boton">3</div>
+					<div onClick={() => { updateCalc('+') }} className="boton parte_roja">+</div>
 
 				</div>
 
 				<div className="fila">
 
-					<div className="boton"><VscDebugRestart /></div>
-					<div className="boton">0</div>
-					<div className="boton">.</div>
-					<div className="boton parte_roja">=</div>
+
+					{/* Boton de rehacer */}
+					<div onClick={borrarUltimo} className="boton"><VscDebugRestart /></div>
+
+					<div onClick={() => { updateCalc('0') }} className="boton">0</div>
+					<div onClick={() => { updateCalc('.') }} className="boton">.</div>
+
+					{/* Boton de igual */}
+					<div onClick={() => calcular()} className="boton parte_roja">=</div>
 
 				</div>
 
